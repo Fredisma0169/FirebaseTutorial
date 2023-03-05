0a0a44dcd941a75f87b4314e2c6471968646b90c
Fredisma0169 <97203352+Fredisma0169@users.noreply.github.com>
2023-03-05 14:09:19 +0100

    Initial Commit

---
+++ FirebaseTutorial/FirebaseTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTutorialApp.swift
+//  FirebaseTutorial
+//
+//  Created by Fredisma OUEDRAOGO on 05/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
